4bd403ecaca66959a71421802b049db2db36588d
wolf1131 <adinamber1883@gmail.com>
2023-04-07 20:34:04 +0300

    dashboard

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'pending'
+    },
+    {
+        productName: 'Larvender Kf102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Drone With Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Gps 4K Drone',
+        productNumber: '22821',
+        paymentStatus: 'paid',
+        shipping: 'delivered'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Lozege  Drone',
+        productNumber: '00482',
+        paymentStatus: 'paid',
+        shipping: 'Delivered'
+    }
+]
